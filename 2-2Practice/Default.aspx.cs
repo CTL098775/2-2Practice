db0020a621bcd0890132ef3693703171fc50884f
CTL098775 <j747300k@gmail.com>
2021-03-09 17:04:43 +0800

    閏年

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,15 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int i_N = 700;
+            if ((i_N % 4) == 0 || (i_N % 400) == 0 && (i_N % 100 )!= 0)
+                {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
